a4c7867ad1a36d886abe0f5cd500f5f35b942617
Kilameen <Kilameen@yandex.ru>
2025-07-17 12:39:14 +0300

    ewm-main-svc Продолжаем бороться с ошибками

--- stat-svc/stats-server/src/main/java/ru/practicum/stat/exception/ErrorHandler.java
+++ stat-svc/stats-server/src/main/java/ru/practicum/stat/exception/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.stat.exception;
 
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,5 +15,18 @@ public class ErrorHandler {
     @ResponseStatus(BAD_REQUEST)
     public ErrorResponse handleConstraintViolationException(ConstraintViolationException ex) {
         return new ErrorResponse("BAD_REQUEST", ex.getMessage());
+    }
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handlerValidationException(Throwable e) {
+        String errorMessage = "Произошла внутренняя ошибка сервера: ";
+        errorMessage += "Тип исключения - " + e.getClass().getSimpleName() + ". ";
+        if (e.getMessage() != null && !e.getMessage().isEmpty()) {
+            errorMessage += "Сообщение: " + e.getMessage();
+        } else {
+            errorMessage += "Сообщение отсутствует.";
+        }
+        return new ErrorResponse("INTERNAL_SERVER_ERROR",errorMessage);
     }
 }
